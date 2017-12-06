ec06f2d337a13309adbb1cacf7d35fd3d4198be7
WitFlash <witflash@gmail.com>
2017-12-06 18:46:46 +0200

    add scss rules and folders

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'), // Подключаем Gulp
 	browserSync = require('browser-sync'); // Подключаем Browser Sync
 
 gulp.task('scss', function () { // Создаем таск "scss"
-	return gulp.src('app/scss/**/*.scss') // Берем все файлы scss из папки и подпапок
+	return gulp.src('app/scss/style.scss') // Берем все файлы scss из папки и подпапок
 		.pipe(scss()) // Преобразовываем Scss в CSS посредством gulp-scss
 		.pipe(gulp.dest('app/css')) // Вывод результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице
@@ -20,7 +20,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('watch', ['browser-sync', 'scss'], function() {
-	gulp.watch('app/scss/**/*.scss', ['scss']); // Наблюдение за scss файлами
+	gulp.watch(['app/scss/**/*.scss', 'app/blocks/**/*.scss'], ['scss']); // Наблюдение за scss файлами
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами
 	gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS
 	// Наблюдение за другими типами файлов
