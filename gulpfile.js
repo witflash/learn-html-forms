6d628ff8a0453f1fd77ace12e278e8d8be65e6c8
witflash <witflash@gmail.com>
2017-12-11 01:11:56 +0200

    add nunjucks support

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,35 @@
 var gulp = require('gulp'), // Подключаем Gulp
 	sass = require('gulp-sass'), // Подключаем Sass пакет 
-	browserSync = require('browser-sync'); // Подключаем Browser Sync
+	nunjucks = require('gulp-nunjucks'), // Подключаем nunjucks
+	htmlbeautify = require('gulp-html-beautify'), // Вложенность в html после nunjucks 
+	browserSync = require('browser-sync'), // Подключаем Browser Sync
+	autoprefixer = require('gulp-autoprefixer'); // Автоматическая поддержка разных браузеров
 
-gulp.task('sass', function () { // Создаем таск "sass"
+gulp.task('nunjucks', function () { // Создаем таск для Nunjucks
+	return gulp.src('app/templates/index.html')
+		.pipe(nunjucks.compile())
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({stream: true})) // Обновляем HTML на странице
+});
+
+/* Таск для оформления отступов в html файлах */
+gulp.task('htmlbeautify', function(){ 
+	var options = {
+		"indent_size": 4,
+		"indent_with_tabs": true
+	}
+	gulp.src('app/*.html')
+		.pipe(htmlbeautify(options))
+		.pipe(gulp.dest('app'))
+});
+
+gulp.task('sass', ['nunjucks'], function () { // Создаем таск "sass"
 	return gulp.src('app/sass/style.scss') // Берем основной файл с импортами
-		.pipe(sass()) // Преобразовываем SCSS в CSS посредством gulp-sass
+		.pipe(sass().on('error', function(err) {
+            console.error(err.message);
+            browserSync.notify(err.message, 3000); // Display error in the browser
+            this.emit('end'); // Prevent gulp from catching the error and exiting the watch process
+        })) // Преобразовываем SCSS в CSS посредством gulp-sass
 		.pipe(gulp.dest('app/css')) // Вывод результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице
 });
@@ -19,9 +44,21 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 	});
 });
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
-	gulp.watch(['app/sass/**/*.scss', 'app/blocks/**/*.scss'], ['sass']); // Наблюдение за scss файлами
-	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами
+gulp.task('watch', ['browser-sync', 'nunjucks', 'sass'], function() {
+	gulp.watch(['app/sass/**/*.scss'], ['sass']); // Наблюдение за scss файлами
+	gulp.watch(['app/**/*.html'], ['nunjucks']); // Наблюдение за HTML файлами
 	gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS
 	// Наблюдение за другими типами файлов
+});
+
+/*  ФИНАЛИЗИРУЕМ ПРОЕКТ:
+	- применяем autoprefixer;
+	- применяем htmlbeautify; */
+gulp.task('finalize', ['htmlbeautify'], function() {
+	gulp.src('app/css/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('app/css'))
 });
